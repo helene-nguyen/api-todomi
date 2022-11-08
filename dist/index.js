e1c7af8d9619c4befac29f4795941eac4218ead3
Yumicode <75518128+helene-nguyen@users.noreply.github.com>
2022-11-08 21:30:36 +0100

    ✨ feat : add api security control

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,32 @@ import express from 'express';
 const app = express();
 import debug from 'debug';
 const logger = debug('Entrypoint');
+import helmet from 'helmet';
+app.use(helmet());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE');
+    next();
+});
+app.set('trust proxy', 1);
+import session from 'express-session';
+app.use(session({
+    saveUninitialized: true,
+    resave: true,
+    proxy: true,
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'lax',
+        maxAge: 24 * 60 * 60 * 1000,
+    },
+}));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false,
+}));
 if (process.env.NODE_ENV !== 'test') {
     const PORT = process.env.PORT ?? 3000;
     app.listen(PORT, () => {
--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,51 @@ const app = express();
 import debug from 'debug';
 const logger = debug('Entrypoint');
 
-//~ Launch the server
+//~ Protect API Helmet
+import helmet from 'helmet';
+app.use(helmet());
+
+//~ Cors
+app.use((req: Request, res: Response, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE');
+  next();
+});
+
+//If you have your node.js behind a proxy and are using secure: true, you need to set 'trust proxy' in express
+app.set('trust proxy', 1);
+// trust first proxy if deploy
+
+//~ Session
+import session from 'express-session';
+app.use(
+  session({
+    saveUninitialized: true,
+    resave: true,
+    proxy: true,
+    secret: process.env.SESSION_SECRET!,
+    cookie: {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'lax', // or 'strict'
+      maxAge: 24 * 60 * 60 * 1000, //24 hours
+      //expires : new Date(Date.now() + 60 * 60 * 1000) //1 hour
+    },
+  })
+);
+
+//~ Encoding parsing the body
+//accept Content-type: application/json
+app.use(express.json());
+// accept Content-type: application/x-www-form-urlencoded
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
+//& Launch the server
 if (process.env.NODE_ENV !== 'test') {
   const PORT = process.env.PORT ?? 3000;
 
